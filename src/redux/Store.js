453f784895bb7c537feab2f65d07dbb5cca29190
yuvrajkushwaha49@gmail.com <[200~git config --global user.name yuvrajkushwaha49@gmail.comgit config --global user.email>
2026-02-27 19:43:31 +0530

    Updated Store.js File

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,4 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../features/counter/CunterSlice";
 export const Store = configureStore({
-    reducer:{}
+
+    reducer:{
+        counter: counterSlice,
+    }
 })
